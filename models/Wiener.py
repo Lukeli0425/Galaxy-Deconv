db425fe813cd42a317f36139f7ff89b9707a82b6
Luke Li <lta19@mails.tsinghua.edu.cn>
2022-10-17 10:26:36 +0800

    Create Wiener.py

---
+++ models/Wiener.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+from torch.fft import fft2, ifft2, fftshift, ifftshift
+
+class Wiener(nn.Module):
+    def __init__(self):
+        super(Wiener, self).__init__()
+        
+    def forward(self, y, psf, snr):
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        
+        psf = psf/psf.sum() # normalize PSF
+        x = fftshift(ifft2(fft2(y) / fft2(psf) / (1+1/(fft2(psf).abs()**2 * snr)) )).real
+        return x
+    
+if __name__ == "__main__":
+    model = Wiener()
