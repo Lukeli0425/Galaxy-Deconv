9b52980df2c9fa94515cd74dfc8b32b3955db8a6
Lukeli0425 <lukeli0425@gmail.com>
2022-10-17 23:11:12 -0500

    Update Wiener module

--- models/Wiener.py
+++ models/Wiener.py
@@ -7,9 +7,12 @@ class Wiener(nn.Module):
         super(Wiener, self).__init__()
         
     def forward(self, y, psf, snr):
-        
         psf = psf/psf.sum() # normalize PSF
-        x = fftshift(ifft2(fft2(y) / fft2(psf) / (1+1/(fft2(psf).abs()**2 * snr)) )).real
+        # x = fftshift(ifft2(fft2(y) / fft2(psf) / (1+1/(fft2(psf).abs()**2 * snr)) )).real
+        H = fft2(psf)
+        numerator = torch.conj(H) * fft2(y)
+        divisor = 1/snr + H.abs() ** 2
+        x = fftshift(ifft2(numerator/divisor)).real
         return x
     
 if __name__ == "__main__":
