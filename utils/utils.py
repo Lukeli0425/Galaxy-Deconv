620cb7f01398379c1609196c5c5c54322cffdfce
Luke Li <lta19@mails.tsinghua.edu.cn>
2023-01-08 19:27:57 +0800

    Add elli estimation

--- utils/utils.py
+++ utils/utils.py
@@ -2,9 +2,10 @@ import os
 import numpy as np
 import logging
 import fpfs
+import galsim
+# import score.cadmos_lib as cl
 
-
-def PSNR(img1, img2, normalize=True):
+def PSNR(img1, img2, normalize=False):
     """Calculate the PSNR of two images."""
     if not img1.shape == img2.shape:
         logging.raiseExceptions('Images have inconsistent Shapes!')
@@ -49,10 +50,16 @@ def estimate_shear(obs, psf_in=False, use_psf=True, beta=0.5):
     else:
         psf = psf_in
         
-    fpTask = fpfs.fpfsBase.fpfsTask(psf, beta=beta)
-    modes = fpTask.measure(obs)
-    ells = fpfs.fpfsBase.fpfsM2E(modes, const=1, noirev=False)
-    resp = ells['fpfs_RE'][0]
+    # fpTask = fpfs.fpfsBase.fpfsTask(psf, beta=beta)
+    # modes = fpTask.measure(obs)
+    # ells = fpfs.fpfsBase.fpfsM2E(modes, const=1, noirev=False)
+    # resp = ells['fpfs_RE'][0]
+    
+    fpTask =  fpfs.image.measure_source(psf, noiFit=None, sigma_arcsec=0.7, pix_scale=0.2, nnord=4)
+    mms = fpTask.measure(obs-obs.min())
+    ells = fpfs.catalog.fpfsM2E(mms,const=2000,noirev=False)
+    resp = ells['fpfs_R1E'][0]
+    
     g_1 = ells['fpfs_e1'][0] / resp
     g_2 = ells['fpfs_e2'][0] / resp
     g = np.sqrt(g_1 ** 2 + g_2 ** 2)
@@ -61,5 +68,93 @@ def estimate_shear(obs, psf_in=False, use_psf=True, beta=0.5):
 
     return (g_1, g_2, g) 
 
+def estimate_elli(obs):
+    # obs -= obs.mean()
+    n_row, n_col = obs.shape
+    U = makeUi(n_row, n_col)
+    GX = np.array([scal(obs, U_i) for U_i in U])
+    mu20 = 0.5*(GX[3] + GX[4]) - GX[0]**2 / GX[2]
+    mu02 = 0.5*(GX[3] - GX[4]) - GX[1]**2 / GX[2]
+    mu11 = GX[5] - GX[0] * GX[1] / GX[2]
+    e1 = (mu20-mu02) / (mu20+mu02)
+    e2 = 2*(mu11) / (mu20+mu02)
+    e = np.sqrt(e1**2 + e2**2)
+    shear = galsim.Shear(e1=e1, e2=e2)
+    
+    return (shear.g1, shear.g2, shear.g)
 
 
+def makeU1(n,m):
+    """This function returns a n x m numpy array with (i)_{i,j} entries where i
+    is the ith line and j is the jth column.
+    INPUT: n positive integer (number of lines),
+           m positive (integer number of columns)
+    OUTPUT: n x m numpy array"""
+    U1 = np.tile(np.arange(n),(m,1)).T
+    return U1
+
+def makeU2(n,m):
+    """This function returns a n x m numpy array with (j)_{i,j} entries where i
+    is the ith line and j is the jth column.
+    INPUT: n positive integer (number of lines),
+           m positive (integer number of columns)
+    OUTPUT: n x m numpy array"""
+    U2 = np.tile(np.arange(m),(n,1))
+    return U2
+
+def makeU3(n,m):
+    """This function returns a n x m numpy array with (1)_{i,j} entries where i
+    is the ith line and j is the jth column.
+    INPUT: n positive integer (number of lines),
+           m positive (integer number of columns)
+    OUTPUT: n x m numpy array"""
+    U3 = np.ones((n,m))
+    return U3
+
+def makeU4(n,m):
+    """This function returns a n x m numpy array with (i^2+j^2)_{i,j} entries 
+    where i is the ith line and j is the jth column.
+    INPUT: n positive integer (number of lines),
+           m positive (integer number of columns)
+    OUTPUT: n x m numpy array"""
+    U4 = np.add.outer(np.arange(n)**2,np.arange(m)**2)
+    return U4
+
+def makeU5(n,m):
+    """This function returns a n x m numpy array with (i^2-j^2)_{i,j} entries 
+    where i is the ith line and j is the jth column.
+    INPUT: n positive integer (number of lines),
+           m positive (integer number of columns)
+    OUTPUT: n x m numpy array"""
+    U5 = np.subtract.outer(np.arange(n)**2,np.arange(m)**2)
+    return U5
+
+def makeU6(n,m):
+    """This function returns a n x m numpy array with (i*j)_{i,j} entries where
+    i is the ith line and j is the jth column.
+    INPUT: n positive integer (number of lines),
+           m positive (integer number of columns)
+    OUTPUT: n x m numpy array"""
+    U6 = np.outer(np.arange(n),np.arange(m))
+    return U6
+
+def makeUi(n,m):
+    """This function returns a 6 x n x m numpy array containing U1, U2, U3, U4,
+    U5 and U6.
+    INPUT: n positive integer (number of lines),
+           m positive (integer number of columns)
+    OUTPUT: 6 x n x m numpy array"""
+    U1 = makeU1(n,m)
+    U2 = makeU2(n,m)
+    U3 = makeU3(n,m)
+    U4 = makeU4(n,m)
+    U5 = makeU5(n,m)
+    U6 = makeU6(n,m)
+    return np.array([U1,U2,U3,U4,U5,U6])
+
+def scal(a,b):
+    """This function returns the scalar product of a and b.
+    INPUT: a, numpy array
+           b, numpy array
+    OUTPUT: scalar"""
+    return (a*np.conjugate(b)).sum()
