fb0e7e08ce0fda1b805c86db6ce088852a9487b3
Tianao Li <lukeli0425@gmail.com>
2023-01-31 00:01:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -5,104 +5,73 @@ import os
 import torch
 from torch.optim import Adam
 
+from models.ResUNet import ResUNet
 from models.Tikhonet import Tikhonet
 from models.Unrolled_ADMM import Unrolled_ADMM
-from models.ResUNet import ResUNet
 from utils.utils_data import get_dataloader
 from utils.utils_plot import plot_loss
-from utils.utils_torch import MultiScaleLoss, ShapeConstraint
+from utils.utils_train import MultiScaleLoss, ShapeConstraint, get_model_name
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = '2'
 
-def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, filter='Identity',
-          n_epochs=10, lr=1e-4, loss='Default',
+def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, filter='Laplacian',
+          n_epochs=10, lr=1e-4, loss='MultiScale',
           data_path='/mnt/WD6TB/tianaoli/dataset/LSST_23.5/', train_val_split=0.8, batch_size=32,
           model_save_path='./saved_models/', pretrained_epochs=0):
-
+    
+    model_name = get_model_name(model_name=model_name, loss=loss, filter=filter, n_iters=n_iters, llh=llh, PnP=PnP)
     logger = logging.getLogger('Train')
-    train_loader, val_loader = get_dataloader(data_path=data_path, train=True, train_test_split=train_val_split, batch_size=batch_size)
+    logger.info(f'Start training {model_name} on {data_path} data for {n_epochs} epochs.')
     
     if not os.path.exists(model_save_path):
         os.mkdir(model_save_path)
-        
+    
+    train_loader, val_loader = get_dataloader(data_path=data_path, train=True, train_test_split=train_val_split, batch_size=batch_size)
+    
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
     if 'ADMM' in model_name:
-        logger.info(f'Start training Unrolled {"PnP-" if PnP else ""}ADMM with {llh} likelihood on {data_path} data for {n_epochs} epochs.')
         model = Unrolled_ADMM(n_iters=n_iters, llh=llh, PnP=PnP)
-        model.to(device)
-        if pretrained_epochs > 0:
-            try:
-                pretrained_file = os.path.join(model_save_path, f'{llh}{"_PnP" if PnP else ""}_{n_iters}iters_MSE_{pretrained_epochs}epochs.pth')
-                model.load_state_dict(torch.load(pretrained_file, map_location=torch.device(device)))
-                logger.info(f'Successfully loaded in {pretrained_file}')
-            except:
-                logger.critical(f'Failed loading in {pretrained_file}')
-        loss_fn = MultiScaleLoss()
     elif 'Tikhonet' in model_name:
-        logger.info(f'Start training Tikhonet with {filter} filter on {data_path} data for {n_epochs} epochs.')
         model = Tikhonet(filter=filter)
-        model.to(device)
-        if pretrained_epochs > 0:
-            try:
-                pretrained_file = os.path.join(model_save_path, f'Tikhonet_{filter}_{pretrained_epochs}epochs.pth')
-                model.load_state_dict(torch.load(pretrained_file, map_location=torch.device(device)))
-                logger.info(f'Successfully loaded in {pretrained_file}')
-            except:
-                logger.critical(f'Failed loading in {pretrained_file}')
-        loss_fn = torch.nn.MSELoss()
     elif model_name == 'ShapeNet':
-        logger.info(f'Start training ShapeNet on {data_path} data for {n_epochs} epochs.')
-        model = Tikhonet(filter='Laplacian')
-        model.to(device)
-        if pretrained_epochs > 0:
-            try:
-                pretrained_file = os.path.join(model_save_path, f'ShapeNet_{pretrained_epochs}epochs.pth')
-                model.load_state_dict(torch.load(pretrained_file, map_location=torch.device(device)))
-                logger.info(f'Successfully loaded in {pretrained_file}')
-            except:
-                logger.critical(f'Failed loading in {pretrained_file}')
-        loss_fn = ShapeConstraint(device=device, fov_pixels=48, n_shearlet=2)
+        model = Tikhonet(filter=filter)
     elif model_name == 'ResUNet':
-        logger.info(f'Start training ResUNet on {data_path} data for {n_epochs} epochs.')
         model = ResUNet()
-        model.to(device)
-        if pretrained_epochs > 0:
-            try:
-                pretrained_file = os.path.join(model_save_path, f'ResUNet_{pretrained_epochs}epochs.pth')
-                model.load_state_dict(torch.load(pretrained_file, map_location=torch.device(device)))
-                logger.info(f'Successfully loaded in {pretrained_file}')
-            except:
-                logger.critical(f'Failed loading in {pretrained_file}')
-        loss_fn = MultiScaleLoss()
 
-    if loss == 'MSE':
+    model.to(device)
+    if pretrained_epochs > 0:
+        try:
+            pretrained_file = os.path.join(model_save_path, f'{model_name}_{pretrained_epochs}epochs.pth')
+            model.load_state_dict(torch.load(pretrained_file, map_location=torch.device(device)))
+            logger.info(f'Successfully loaded in {pretrained_file}.')
+        except:
+            logger.critical(f'Failed loading in {pretrained_file}!')
+
+    if 'ShapeNet' in model_name or loss == 'Shape':
+        loss_fn = ShapeConstraint(device=device, fov_pixels=48, n_shearlet=2)
+    elif loss == 'MSE':
         loss_fn = torch.nn.MSELoss()
     elif loss == 'MultiScale':
         loss_fn = MultiScaleLoss()
-    elif loss == 'ShapeConstraint':
-        loss_fn = ShapeConstraint(device=device, fov_pixels=48, n_shearlet=2)
-
-    model_name = f'{llh}{"_PnP" if PnP else ""}_{loss if not loss=="Default" else ""}_{n_iters}iters' if 'ADMM' in model_name else (f'{model_name}_{filter}' if model_name=='Tikhonet' else model_name)
     
     optimizer = Adam(params=model.parameters(), lr = lr)
 
-    train_loss_list = []
-    val_loss_list = []
+    train_loss_list, val_loss_list = [], []
     for epoch in range(n_epochs):
         model.train()
         train_loss = 0.0
         for idx, ((obs, psf, alpha), gt) in enumerate(train_loader):
             optimizer.zero_grad()
             obs, psf, alpha, gt = obs.to(device), psf.to(device), alpha.to(device), gt.to(device)
-            # rec = model(obs/alpha) * alpha
             rec = model(obs, psf, alpha)
             loss = loss_fn(gt, rec)
 
             loss.backward()
             optimizer.step()
-            train_loss += loss.item()
+            train_loss = loss.item()
             
-            # Evaluate on valid dataset
+            # Evaluate on valid dataset.
             if (idx+1) % 25 == 0:
                 val_loss = 0.0
                 model.eval()
@@ -115,10 +84,10 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, filter
 
                 logger.info(" [{}: {}/{}]  train_loss={:.4f}  val_loss={:.4f}".format(
                                 epoch+1, idx+1, len(train_loader),
-                                train_loss/(idx+1),
+                                train_loss/25,
                                 val_loss/len(val_loader)))
     
-        # Evaluate on train & valid dataset after every epoch
+        # Evaluate on train & valid dataset after every epoch.
         train_loss = 0.0
         model.eval()
         with torch.no_grad():
@@ -141,18 +110,18 @@ def train(model_name='Unrolled ADMM', n_iters=8, llh='Poisson', PnP=True, filter
 
         logger.info(" [{}: {}/{}]  train_loss={:.4f}  val_loss={:.4f}".format(
                         epoch+1, len(train_loader), len(train_loader),
-                        train_loss/(idx+1),
+                        train_loss/len(train_loader),
                         val_loss/len(val_loader)))
-
+        
+        # Save model.
         if (epoch + 1) % 5 == 0:
             model_file_name = f'{model_name}_{epoch+1+pretrained_epochs}epochs.pth'
             torch.save(model.state_dict(), os.path.join(model_save_path, model_file_name))
             logger.info(f'Model saved to {os.path.join(model_save_path, model_file_name)}')
 
-        # Plot loss curve
+        # Plot loss curve.
         plot_loss(train_loss_list, val_loss_list, model_save_path, model_name)
 
-    return
 
 
 if __name__ == "__main__":
@@ -165,14 +134,12 @@ if __name__ == "__main__":
     parser.add_argument('--filter', type=str, default='Identity', choices=['Identity', 'Laplacian'])
     parser.add_argument('--n_epochs', type=int, default=50)
     parser.add_argument('--lr', type=float, default=1e-4)
-    parser.add_argument('--loss', type=str, default='MultiScale', choices=['Default', 'MultiScale', 'MSE', 'ShapeConstraint'])
+    parser.add_argument('--loss', type=str, default='MultiScale', choices=['Default', 'MultiScale', 'MSE', 'Shape'])
     parser.add_argument('--train_val_split', type=float, default=0.8)
     parser.add_argument('--batch_size', type=int, default=32)
     parser.add_argument('--pretrained_epochs', type=int, default=0)
     opt = parser.parse_args()
 
-    # from time import sleep
-    # sleep(3600*2)
 
     train(model_name=opt.model, n_iters=opt.n_iters, llh=opt.llh, PnP=True, filter=opt.filter,
           n_epochs=opt.n_epochs, lr=opt.lr, loss=opt.loss,
